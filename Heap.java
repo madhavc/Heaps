c0d408b166253fd8b712f140b27237392e898454
Madhav Chhura <chhura.madhav@gmail.com>
2014-04-22 10:49:24 -0700

    Update Heap.java

--- Heap.java
+++ Heap.java
@@ -1,10 +1,5 @@
 /**
- * 
- */
-package edu.csupomona.cs.cs241.prog_assigmnt_1;
-
-/**
- * @author madhavc
+ * @author Madhav Chhura
  *
  */
 public interface Heap<V extends Comparable<V>> {
@@ -12,15 +7,15 @@ public interface Heap<V extends Comparable<V>> {
 	public static enum MODE {MAX, MIN};
 
 	/**
-	   * Adds an entry to the heap, sorting the heap for priority afterwards.
-	   * @param entry of generic type the entry to be added.
-	   */
+	  * Adds an entry to the heap, sorting the heap for priority afterwards.
+	  * @param entry of generic type the entry to be added.
+	  */
 	public void add(V value);
 
-    /**
-     * Creates a new heap using an array
-     * @param array heap values in the array.
-     */
+    	/**
+     	  * Creates a new heap using an array
+     	  * @param array heap values in the array.
+     	  */
 	public void fromArray(V[] array);
 
 	/**
@@ -35,22 +30,22 @@ public interface Heap<V extends Comparable<V>> {
 
 	
 	/**
-     * Removes the top of the heap (highest priority item, root)
-     * @return The top of the heap
-     */
+     	  * Removes the top of the heap (highest priority item, root)
+          * @return The top of the heap
+          */
 	public V remove();
 	
 	/**
 	 * This method is used to set the mode of the Heap 
 	 * Only two options MAX or MIN
-     * @param mode MAX or MIN
+         * @param mode MAX or MIN
 	 */
 	public void setMode(MODE mode);
 	
 	/**
-     * Converts a heap into an array of generic type.
-     * @return array of heap values.
-     */
+          * Converts a heap into an array of generic type.
+          * @return array of heap values.
+          */
 	public V[] toArray();
 
 }
